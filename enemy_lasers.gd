76e6e321319dc27863abb31a6f3782071d6eb7a8
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-06-08 18:41:59 +0100

    Find and place objects for burst effect

--- enemy_lasers.gd
+++ enemy_lasers.gd
@@ -1,9 +1,10 @@
 extends RigidBody3D
 
+@onready var bullet_explosion = preload("res://explosion.tscn")
 
 func _physics_process(delta):
 	linear_velocity = global_transform.basis.z * -1 * 350
-
+	#print(global_position)
 
 func _on_timer_timeout():
 	queue_free()
@@ -11,5 +12,12 @@ func _on_timer_timeout():
 
 
 func _on_laser_collision_body_entered(body):
+	var projectile = bullet_explosion.instantiate()
+	#projectile.position = position
+	print(body.to_local(global_position))
+	#projectile.position = (projectile.global_position - body.global_position)
+	projectile.position = body.to_local(%MeshInstance3D2.global_position)
+	body.add_sibling(projectile)
+	
 	queue_free()
 	pass # Replace with function body.
