6a3e8228cb602646f6a60fb487468bb98636c02a
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-06-13 13:44:47 +0100

    Make node names clearer

--- Camera3D.gd
+++ Camera3D.gd
@@ -3,4 +3,5 @@ extends Camera3D
 
 func _on_direction_plane_input_event(camera, event, position, normal, shape_idx):
 	%Projectile_launcher_position.position = position
+	print(position)
 	pass # Replace with function body.
